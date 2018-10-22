3d1fe0d24af72c2596c99bc6618ddb666775a9dd
Rodrigo Queiroz <rqclima@gmail.com>
2018-10-22 17:13:27 -0300

    Speaker

--- eventex/core/tests/test_view_home.py
+++ eventex/core/tests/test_view_home.py
@@ -5,6 +5,7 @@ from django.shortcuts import resolve_url as r
 # Create your tests here.
 
 class HomeTest(TestCase):
+    fixtures = ['keynotes.json']
 
     def setUp(self):
         self.response = self.client.get(r('home'))
@@ -23,8 +24,13 @@ class HomeTest(TestCase):
 
     def test_speakers(self):
         """Must show keynote speakers"""
-        contents = ['Grace Hopper', 'http://hbn.link/hopper-pic',
-                    'Alan Turing', 'http://hbn.link/turing-pic']
+        contents = [
+            'href="{}"'.format(r('speaker_detail', slug='grace-hopper')),
+            'Grace Hopper',
+            'http://hbn.link/hopper-pic',
+            'href="{}"'.format(r('speaker_detail', slug='alan-turing')),
+            'Alan Turing',
+            'http://hbn.link/turing-pic']
 
         for expected in contents:
             with self.subTest():
