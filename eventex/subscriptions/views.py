56da0f2f1bfaaffd4050d4d9df958c837d9b7ec1
Rodrigo Queiroz <rqclima@gmail.com>
2018-10-25 10:39:34 -0300

    Mixins II

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,5 +1,4 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.views.generic import DetailView
 from django.views.generic.edit import CreateView
 
 from eventex.subscriptions.forms import SubscriptionForm
@@ -19,14 +18,4 @@ new = EmailCreateView.as_view(model=Subscription,
                               email_subject='Confirmação de inscrição')
 
 
-# TODO Usar DetailView, ver M4A11, 23min
-# DetailView
-
-def detail(request, hash_id):
-    try:
-        subscription = Subscription.objects.get(hash_id=hash_id)
-    except Subscription.DoesNotExist:
-        raise Http404
-
-    return render(request, 'subscriptions/subscription_detail.html',
-                  {'subscription': subscription})
+detail = DetailView.as_view(model=Subscription, slug_field='hash_id', slug_url_kwarg='hash_id')
